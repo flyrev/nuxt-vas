588bce1aa5229f718f2c0955beaca24369f5c101
Christian Neverdal <flyrev@gmail.com>
2024-11-02 21:32:08 +0100

    Doc

--- webapp/middleware/my-middleware.ts
+++ webapp/middleware/my-middleware.ts
@@ -18,5 +18,15 @@ export default defineNuxtRouteMiddleware((to, from) => {
   if (to.path !== '/') {
     return navigateTo('/')
   }
+
+
+  addRouteMiddleware('global-test', () => {
+    console.log('this global middleware was added in a plugin and will be run on every route change')
+  }, { global: true })
+
+  addRouteMiddleware('named-test', () => {
+    console.log('this named middleware was added in a plugin and would override any existing middleware of the same name')
+  })
+
 })
 
