76b5e7b299ec69f31ed7bcec2be7966ce611ea00
Christian Neverdal <flyrev@gmail.com>
2024-11-02 21:15:54 +0100

    TypeScript

--- webapp/app.vue
+++ webapp/app.vue
@@ -2,7 +2,12 @@
   <div>
     <NuxtRouteAnnouncer />
     <NuxtWelcome />
-    <div>{{ count }}</div>
+    <div>{{ count }} {{ double }}</div>
   </div>
 </template>
 
+<script setup lang="ts">
+/* ref() and computed() are auto-imported */
+const count = ref(1)
+const double = computed(() => count.value * 2)
+</script>
